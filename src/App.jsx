0b12b1db89767b86bba9e9b728bf75147be622dd
Oleksandr Polivanov <aldiker@ukr.net>
2024-07-22 17:16:38 +0300

    Added GlobalStyles, and added some SCC styles in Button and Input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import styled from 'styled-components'
+import GlobalStyle from './styles/GlobalStyles'
+import Button from './ui/Button'
+import Input from './ui/Input'
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+`
+
+const StyleApp = styled.div`
+    background-color: orangered;
+    padding: 20px;
+`
+
 export default function App() {
-    return <div>Hello</div>
+    return (
+        <>
+            <GlobalStyle />
+            <StyleApp>
+                <H1>Hello</H1>
+                <Button onClick={() => alert('Hello')}>Hello</Button>
+                <Button onClick={() => alert('Good Buy')}>Good Buy</Button>
+                <Input type="number" placeholder="Number of guests" />
+            </StyleApp>
+        </>
+    )
 }
