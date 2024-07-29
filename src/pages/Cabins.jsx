1b82b8fe5c483b2bdc5e4d1a9f0ca6ca470c0857
Oleksandr Polivanov <aldiker@ukr.net>
2024-07-29 16:57:58 +0300

    Connected Supabase to app. Started to create apiCabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
+import { getCabins } from '../services/apiCabins'
 
 function Cabins() {
+    // useEffect(function () {
+    //     getCabins().then((data) => console.log(data))
+    // }, [])
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getCabins()
+            console.log(data)
+        }
+        fetchData()
+    }, [])
+
     return (
         <Row type="horizontal">
             <Heading as="h1">All cabins</Heading>
