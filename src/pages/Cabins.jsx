5aeb3033b66d43be4830767900c01373c326782e
Oleksandr Polivanov <aldiker@ukr.net>
2024-08-21 17:01:04 +0300

    Created CabinTable and CabinRow

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,28 +2,18 @@ import { useEffect } from 'react'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
 import { getCabins } from '../services/apiCabins'
+import CabinTable from '../features/cabins/CabinTable'
 
-function Cabins() {
-    useEffect(() => {
-        async function fetchData() {
-            const data = await getCabins()
-            console.log(data)
-        }
-        fetchData()
-    }, [])
-
+export default function Cabins() {
     return (
         <Row type="vertical">
             <Row type="horizontal">
                 <Heading as="h1">All cabins</Heading>
-                <p>TEST</p>
+                <p>Filter / Sort</p>
             </Row>
-            <img
-                src="https://rhcieegpujvlcbaguuvh.supabase.co/storage/v1/object/public/cabins-images/cabin-001.jpg"
-                alt="Cabin 1"
-            />
+            <Row>
+                <CabinTable />
+            </Row>
         </Row>
     )
 }
-
-export default Cabins
