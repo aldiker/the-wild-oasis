92aebe4e1cd4a95bde0a9e90007aefd6f18d658a
Oleksandr Polivanov <aldiker@ukr.net>
2024-08-25 14:40:00 +0300

    Added hot-toast. Integrated Toaster in App.js. Integrated toast.succedd and error in CabinRow

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { formatCurrency } from '../../utils/helpers'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { deleteCabins } from '../../services/apiCabins'
+import toast from 'react-hot-toast'
 
 const TableRow = styled.div`
     display: grid;
@@ -58,20 +59,20 @@ export default function CabinRow({ cabin }) {
     const { isLoading: iaDeleting, mutate } = useMutation({
         mutationFn: deleteCabins,
         onSuccess: () => {
-            alert('Cabin successfully deleted!')
+            toast.success('Cabin successfully deleted!')
 
             queryClient.invalidateQueries({
                 queryKey: ['cabins'],
             })
         },
-        onError: (err) => alert(err.message),
+        onError: (err) => toast.error(err.message),
     })
 
     return (
         <TableRow role="row">
             <Img src={image} />
             <Cabin>{name}</Cabin>
-            <div>Fits up tp {maxCapacity} guests</div>
+            <div>Fits up to {maxCapacity} guests</div>
             <Price>{formatCurrency(regularPrice)}</Price>
             <Discount>{formatCurrency(discount)}</Discount>
             <button onClick={() => mutate(cabinId)} disabled={iaDeleting}>
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -51,9 +51,9 @@ export default function CabinTable() {
                 <div>Discount</div>
                 <div></div>
             </TableHeader>
-            {cabins.map((cabin) => (
+            {/* {cabins.map((cabin) => (
                 <CabinRow cabin={cabin} key={cabin.id} />
-            ))}
+            ))} */}
         </Table>
     )
 }
