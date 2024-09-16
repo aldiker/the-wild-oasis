1a54448e9a6697380737d0a3eef627ed225645bf
Oleksandr Polivanov <aldiker@ukr.net>
2024-09-16 16:50:56 +0300

    Added AddCabin component,

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Button from '../../ui/Button'
+import CreateCabinForm from './CreateCabinForm'
+import Modal from '../../ui/Modal'
+
+export default function AddCabin() {
+    const [isOpenModal, setIsOpenModal] = useState(false)
+
+    return (
+        <div>
+            <Button onClick={() => setIsOpenModal((show) => !show)}>
+                Add new cabin
+            </Button>
+
+            {isOpenModal && <Modal />}
+        </div>
+    )
+}
