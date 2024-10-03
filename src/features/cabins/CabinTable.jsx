d9286b0c14fc63696bdc69d3b8764b6f3e28c449
Oleksandr Polivanov <aldiker@ukr.net>
2024-10-03 16:45:49 +0300

    Added Menus component. Don't complete ...

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,9 +1,8 @@
-import styled from 'styled-components'
-
 import Spinner from '../../ui/Spinner'
 import CabinRow from './CabinRow'
 import { useCabins } from './useCabins'
 import Table from '../../ui/Table'
+import Menus from '../../ui/Menus'
 
 export default function CabinTable() {
     const { isLoading, cabins } = useCabins()
@@ -11,20 +10,24 @@ export default function CabinTable() {
     if (isLoading) return <Spinner />
 
     return (
-        <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-            <Table.Header>
-                <div></div>
-                <div>Cabin</div>
-                <div>Capacity</div>
-                <div>Price</div>
-                <div>Discount</div>
-                <div></div>
-            </Table.Header>
+        <Menus>
+            <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+                <Table.Header>
+                    <div></div>
+                    <div>Cabin</div>
+                    <div>Capacity</div>
+                    <div>Price</div>
+                    <div>Discount</div>
+                    <div></div>
+                </Table.Header>
 
-            <Table.Body
-                data={cabins}
-                render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-            />
-        </Table>
+                <Table.Body
+                    data={cabins}
+                    render={(cabin) => (
+                        <CabinRow cabin={cabin} key={cabin.id} />
+                    )}
+                />
+            </Table>
+        </Menus>
     )
 }
