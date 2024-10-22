d02811d60144ab1fd59fe45e936c0eb2f7b45368
Oleksandr Polivanov <aldiker@ukr.net>
2024-10-22 17:02:30 +0300

    Added filtering and sorting "booking" table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,29 @@
 import { useQuery } from '@tanstack/react-query'
 import { getBookings } from '../../services/apiBookings'
+import { useSearchParams } from 'react-router-dom'
 
 export function useBookings() {
+    const [searchParams] = useSearchParams()
+
+    // FILTER
+    const filterValue = searchParams.get('status')
+    const filter =
+        !filterValue || filterValue === 'all'
+            ? null
+            : { field: 'status', value: filterValue }
+
+    // SORT
+    const sortByRow = searchParams.get('sortBy')
+    const [field, direction] = sortByRow.split('-')
+    const sortBy = { field, direction }
+
     const {
         isError,
         isLoading,
         data: bookings,
     } = useQuery({
-        queryKey: ['bookings'],
-        queryFn: getBookings,
+        queryKey: ['bookings', filter, sortBy],
+        queryFn: () => getBookings({ filter, sortBy }),
     })
 
     return { isLoading, isError, bookings }
