61401dc74512b5ce7b15c5143ee45004deb5a949
Oleksandr Polivanov <aldiker@ukr.net>
2024-11-08 13:41:33 +0200

    Added signup function in apiAuth. Added useSignup hook and implemented it in SignupForm to sigh up new users

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,18 @@ import Button from '../../ui/Button'
 import Form from '../../ui/Form'
 import FormRow from '../../ui/FormRow'
 import Input from '../../ui/Input'
+import { useSignup } from './useSignup'
 
 // Email regex: /\S+@\S+\.\S+/
 
 export default function SignupForm() {
-    const { register, formState, getValues, handleSubmit } = useForm()
+    const { register, formState, getValues, handleSubmit, reset } = useForm()
+    const { signup, isLoading } = useSignup()
+
     const { errors } = formState
 
-    function onSubmit(data) {
-        console.log(data)
+    function onSubmit({ fullName, email, password }) {
+        signup({ fullName, email, password }, { onSettled: () => reset() })
     }
 
     return (
