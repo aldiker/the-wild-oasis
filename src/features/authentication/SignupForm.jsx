484eb619ba7bad53d9180c05c8b4e126ab7e8b53
Oleksandr Polivanov <aldiker@ukr.net>
2024-11-08 16:41:23 +0200

    Added isLoading to disabled attribute into imputs and buttons in SignupForm

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -23,6 +23,7 @@ export default function SignupForm() {
                 <Input
                     type="text"
                     id="fullName"
+                    disabled={isLoading}
                     {...register('fullName', {
                         required: 'This field is required',
                     })}
@@ -33,6 +34,7 @@ export default function SignupForm() {
                 <Input
                     type="email"
                     id="email"
+                    disabled={isLoading}
                     {...register('email', {
                         required: 'This field is required',
                         pattern: {
@@ -50,6 +52,7 @@ export default function SignupForm() {
                 <Input
                     type="password"
                     id="password"
+                    disabled={isLoading}
                     {...register('password', {
                         required: 'This field is required',
                         minLength: {
@@ -67,6 +70,7 @@ export default function SignupForm() {
                 <Input
                     type="password"
                     id="passwordConfirm"
+                    disabled={isLoading}
                     {...register('passwordConfirm', {
                         required: 'This field is required',
                         validate: (value) =>
@@ -78,10 +82,10 @@ export default function SignupForm() {
 
             <FormRow>
                 {/* type is an HTML attribute! */}
-                <Button variation="secondary" type="reset">
+                <Button variation="secondary" type="reset" disabled={isLoading}>
                     Cancel
                 </Button>
-                <Button>Create new user</Button>
+                <Button disabled={isLoading}> Create new user</Button>
             </FormRow>
         </Form>
     )
