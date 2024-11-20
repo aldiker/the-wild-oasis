01bcfa11fa68f07f51db9f5ef4fcd69f161eeb57
Oleksandr Polivanov <aldiker@ukr.net>
2024-11-20 17:01:25 +0200

    Installed Recharts library. Created SalesChart and created the base chart with fakedata

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import { useRecentStays } from './useRecentStays'
 import Spinner from '../../ui/Spinner'
 import Stats from './Stats'
 import { useCabins } from '../cabins/useCabins'
+import SalesChart from './SalesChart'
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -37,7 +38,7 @@ export default function DashboardLayout() {
             />
             <div>Today&apos;s activity</div>
             <div>Chart stay durations</div>
-            <div>Chart sales</div>
+            <SalesChart />
         </StyledDashboardLayout>
     )
 }
