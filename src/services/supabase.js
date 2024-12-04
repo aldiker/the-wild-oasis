795eccf16737eaad866b617c9cb3371401f61224
Oleksandr Polivanov <aldiker@ukr.net>
2024-12-04 12:34:46 +0200

    Created virtual enviroment file .env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseUrl = 'https://rhcieegpujvlcbaguuvh.supabase.co'
-// const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoY2llZWdwdWp2bGNiYWd1dXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5MDk3NzksImV4cCI6MjAzNzQ4NTc3OX0.nRoLgDK96BPOxgOl2oCDMrFUqKQnnSk9ztSkVVK_DIA'
+
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+console.log('supabaseKey:', supabaseKey)
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
